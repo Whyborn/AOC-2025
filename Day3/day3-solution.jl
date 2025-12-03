f519cb86187d8f84c988b1688b3917c1fceebd19
Whyborn <lachlan.s.whyborn@gmail.com>
2025-12-03 18:58:26 +1100

    Use views for slight performance improvement

--- Day3/day3-solution.jl
+++ Day3/day3-solution.jl
@@ -1,21 +1,24 @@
+using BenchmarkTools
+
 function max_joltage(banks, N)
     sum = 0
-    for bank in banks
-        joltages = [parse(Int, c) for c in bank]
-        max_joltage = ""
-        for i = 1:N
-            joltage, loc = findmax(joltages[1:end-(N-i)])
-            max_joltage *= string(joltage)
-            joltages = joltages[loc+1:end]
+    @views begin
+        for bank in banks
+            joltages = [parse(Int, c) for c in bank]
+            max_joltage = ""
+            for i = 1:N
+                joltage, loc = findmax(joltages[1:end-(N-i)])
+                max_joltage *= string(joltage)
+                joltages = joltages[loc+1:end]
+            end
+            sum += parse(Int, max_joltage)
         end
-        sum += parse(Int, max_joltage)
     end
     sum
 end
 
 begin
     inp = readlines("day3-input.txt")
-
     println("Part 1 solution: $(max_joltage(inp, 2))")
     println("Part 2 solution: $(max_joltage(inp, 12))")
 end
